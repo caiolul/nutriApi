8fe095f3554fdc5128a1b1428efaff42e9cf08e4
caiolul <caiolucena.il@gmail.com>
2020-08-24 00:12:44 -0300

    feat: Deploy heroku

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
+import dotenv from 'dotenv';
 import 'reflect-metadata';
 import express from 'express';
 import cors from 'cors';
 import routes from './routes';
 import './database';
+
+dotenv.config();
 
 const app = express();
 
@@ -11,5 +14,5 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(3333, () => {
-  console.log('http://localhost:3333');
+    console.log('Running server');
 });
